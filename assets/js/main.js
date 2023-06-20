ee3aa1e1f9f183e9c96ef2922349551bc669f58e
bguz <betoguz7@gmail.com>
2023-06-19 19:18:33 -0500

    most of html, css, and js done, needs media queries and testig

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+document.querySelector('.rating__submit').addEventListener('click', test);
+
+function test(e) {
+    const getValue = document.querySelector('input[name="rating"]:checked');
+    const ratingContainer = document.querySelector('.rating');
+    const thanksContainer = document.querySelector('.thanks');
+    const ratingMessage = document.querySelector('.thanks__rating-selected');
+
+    if (getValue !== null) {
+        console.log(getValue.value);
+        ratingContainer.classList.add('hidden');
+        thanksContainer.classList.remove('hidden');
+        ratingMessage.textContent = `You selected ${getValue.value} out of 5`;
+    } else {
+        e.preventDefault();
+    }
+}
+
+// MODIFY FONT SIZES FOR MOBILE AND DO MEDIA QUERIES AND REFACTOR
